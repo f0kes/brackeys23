d0dcbbc8803f57663619b6f3a7194852f9b065fa
xslix <slifromtaraz@gmail.com>
2023-08-26 14:39:18 +0300

    Еще исправления звуков, добавил звук личинки

---
+++ Assets/Scripts/Character/Enemy/LeechSoundController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Characters.Enemy
+{
+    public class LeechSoundController : MonoBehaviour
+    {
+        [SerializeField] private Character character;
+        [SerializeField] private AudioClip[] spawnAudioClips;
+        
+        private void Start()
+        {
+            character.GetAudioSource().PlayOneShot(spawnAudioClips[Random.Range(0, spawnAudioClips.Length)]);
+        }
+    }
+}
